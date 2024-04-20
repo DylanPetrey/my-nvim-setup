5225ad720ae9889a69b89a8530c4b5e958efbd99
DylanPetrey <65378181+DylanPetrey@users.noreply.github.com>
2024-04-20 12:50:15 -0500

    LSP

--- lua/dylan/lazy.lua
+++ lua/dylan/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dylan.plugins", {
+require("lazy").setup({ { import = "dylan.plugins" }, { import = "dylan.plugins.lsp" } }, {
 				checker = {
 								enabled = true,
 								notify = false,
