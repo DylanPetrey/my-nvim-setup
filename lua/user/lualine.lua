0f140315103dc486195e09b2d444203f70dca2c5
DylanPetrey <65378181+DylanPetrey@users.noreply.github.com>
2024-04-22 21:57:28 -0500

    Add LSP to lualine. reset bufferline change

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -48,6 +48,25 @@ local location = {
 	padding = 0,
 }
 
+local lsp = {
+	-- Lsp server name .
+	function()
+		local msg = "No Active Lsp"
+		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+		local clients = vim.lsp.get_active_clients()
+		if next(clients) == nil then
+			return msg
+		end
+		for _, client in ipairs(clients) do
+			local filetypes = client.config.filetypes
+			if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+				return client.name
+			end
+		end
+		return msg
+	end,
+}
+
 local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
@@ -64,9 +83,9 @@ lualine.setup({
 	sections = {
 		lualine_a = { branch, diagnostics },
 		lualine_b = { mode },
-		lualine_c = {},
+		lualine_c = { },
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_x = { lsp, diff },
 		lualine_y = { location },
 		lualine_z = { "progress" },
 	},
